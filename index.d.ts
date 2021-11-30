5c96ab194d57c8451f5a39517b57063823b2e746
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-11-30 12:29:23 +0300

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module "bastools"
+export function Random(input?: Array<String>): Array<String>
+export function RandomIDGen(length?: number): String
+export function DupCheck(check?: String, whatToCheck?: String): Boolean
+export function StringToNumber(Input?: String): Number
+export function RemoveDup(Input?: Array<String>): Array<String>
