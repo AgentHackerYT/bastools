65ac2f31ee1dce204bcd4a23f33fac43b9306f74
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2022-02-21 15:52:01 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,23 @@
 declare module "bastools"
-export function Random(input?: Array<String>): Array<String>
-export function RandomIDGen(length?: number): String
-export function DupCheck(check?: String, whatToCheck?: String): Boolean
-export function StringToNumber(Input?: String): Number
-export function RemoveDup(Input?: Array<String>): Array<String>
+import { TimerOptions } from "timers"
+export function Random(input?: Array<string>): Array<string>
+/** 
+ * @deprecated Use RandomString Function Instead
+ */
+export function RandomIDGen(length?: number): string
+export function DupCheck(check?: string, whatToCheck?: string): Boolean
+export function StringToNumber(Input?: string): Number
+export function RemoveDup(Input?: Array<string>): Array<string>
+export function firstLetterToUppercase(text: string): string
+export function firstLetterOfEveryWordToUppercase(text: string): string
+export function lastLetterToUppercase(text: string): string
+export function lastLetterOfEveryWordToUppercase(text: string): string
+export function ArrayToString(array: Array<string>, separator?: String): string
+export function StringToArray(text: Array<string>, separator?: String): Array<string>
+export function RandomString(length?: number): string
+export function getFutureTime(howManyDays: number): string
+export function wait(delay?: number, value?: void, options?: TimerOptions): Promise<void>
+export function allLettersUppercaseExecptFirstLetterForEveryWord(text: string): string
+export function allLettersUppercaseExecptFirstLetter(text: string): string
+export function encryptBase64(text: string): string
+export function decryptBase64(text: string): string
